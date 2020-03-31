8b84fb6996bc19a1bbe5c4ea98f1d053d66e9eba
Aleph Santos Oliveira <aleph@ciandt.com>
2020-03-31 10:24:49 -0300

    setup project

---
+++ src/main/java/com/propertiesplay/PropertiesplayApplication.java
@@ -0,0 +1,13 @@
+package com.propertiesplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertiesplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertiesplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/propertiesplay/PropertiesplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.propertiesplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertiesplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
